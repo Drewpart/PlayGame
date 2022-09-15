e589257bc68e635ad3f43a548bafa6a18a212c37
andrpart669 <andrpart669@SDS-L-H85HC63>
2022-09-15 10:59:49 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PlayGame
+{
+    partial class PlayGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.strtButton = new System.Windows.Forms.Button();
+            this.backGround = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("MS Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(22, 130);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(217, 26);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // strtButton
+            // 
+            this.strtButton.Font = new System.Drawing.Font("MS PGothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strtButton.ForeColor = System.Drawing.Color.White;
+            this.strtButton.Location = new System.Drawing.Point(95, 27);
+            this.strtButton.Name = "strtButton";
+            this.strtButton.Size = new System.Drawing.Size(89, 56);
+            this.strtButton.TabIndex = 1;
+            this.strtButton.Text = "START";
+            this.strtButton.UseVisualStyleBackColor = true;
+            this.strtButton.Click += new System.EventHandler(this.strtButton_Click);
+            // 
+            // backGround
+            // 
+            this.backGround.BackColor = System.Drawing.Color.Transparent;
+            this.backGround.Location = new System.Drawing.Point(0, 0);
+            this.backGround.Name = "backGround";
+            this.backGround.Size = new System.Drawing.Size(309, 298);
+            this.backGround.TabIndex = 2;
+            // 
+            // PlayGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(283, 227);
+            this.Controls.Add(this.strtButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.backGround);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Name = "PlayGame";
+            this.Text = "Play Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button strtButton;
+        private System.Windows.Forms.Label backGround;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class PlayGame : Form
+    {
+        int num = 5;
+        int num2 = 4;
+        int num3 = 3;
+        int num4 = 2;
+        int num5 = 1;
+        public PlayGame()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void strtButton_Click(object sender, EventArgs e)
+        {
+            backGround.BackColor = Color.Black;
+            outputLabel.BackColor = Color.Black;
+            outputLabel.Text = $"Game will start in: {num}";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = $"Game will start in: {num2}";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = $"Game will start in: {num3}";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = $"Game will start in: {num4}";
+            Refresh();
+            Thread.Sleep(1000);
+            outputLabel.Text = $"Game will start in: {num5}";
+            Refresh();
+            Thread.Sleep(1000);
+            backGround.BackColor = Color.DarkGreen;
+            outputLabel.BackColor = Color.DarkGreen;
+            outputLabel.Text = "GO!!!!!";
+
+        }
+        }
+    }
